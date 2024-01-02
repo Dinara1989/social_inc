05161a1a256bf96b3e10eae9ec5b1cd9ea38e746
Dinara <dinara198989@bk.ru>
2024-01-02 14:53:18 +0100

    Components. Lesson 13

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABSta4ztO2Z73YCEvZDFgCPesndhqt-seBg&usqp=CAU"
+             alt="logo"></img>
+    </header>
+}
+
+export default Header;
