e9a8f4271d8251f3f44650ec0dfca3a668d70c1a
Dinara <dinara198989@bk.ru>
2024-01-02 12:34:31 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello, samurai! Let's go!!!!!!!!</h1>
+        <h1>Hello, samurai! Let's go!!!!!!!! Finally</h1>
     </div>
   );
 }
